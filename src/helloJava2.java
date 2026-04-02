c32eacdd06b9c45a8ab6ff0ccc4e6fbafb247ae4
hoorak17 <hoorak17@naver.com>
2026-04-02 23:48:57 +0900

    2-6 자바 프로그램 실행

---
+++ src/helloJava2.java
@@ -0,0 +1,8 @@
+public class helloJava2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello java1");
+        System.out.println("Hello java2");
+        System.out.println("Hello java3");
+    }
+}
