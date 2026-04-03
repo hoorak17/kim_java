e88ef8a79a8bad5ab938ad9a906f7debaa945950
hoorak17 <hoorak17@naver.com>
2026-04-03 23:44:56 +0900

    챕터1 helloworld끝

---
+++ src/CommentJava.java
@@ -0,0 +1,11 @@
+public class CommentJava {
+
+    public static void main(String[] args) {//psvm약자 입력으로 한번에 만들어짐.
+        System.out.println("Hello java1");//hello java 1을 출력한다.
+        //Sys~~~~
+
+        /*
+        System~~~~~~
+         */
+    }
+}
