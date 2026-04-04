69c9b68b3a4f9830114e4bf37a25597dfb50981e
hoorak17 <hoorak17@naver.com>
2026-04-04 12:42:20 +0900

    Var

---
+++ src/variable/Var1.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var1 {
+    static void main() {
+        System.out.println(10);//한군데씩 바꾸기 귀찮으니까.
+        System.out.println(10);
+        System.out.println(10);
+
+    }
+}
---
+++ src/variable/Var2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var2 {
+    static void main(String[] args) {
+        int a;
+         a= 10;
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
