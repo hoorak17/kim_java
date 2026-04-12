e0a8733c069d27b7ae46fd3581d3eff157f64dbf
hoorak17 <hoorak17@naver.com>
2026-04-12 23:22:31 +0900

    0412

---
+++ src/condition/if1.java
@@ -0,0 +1,17 @@
+package condition;
+
+public class if1 {
+
+    static void main() {
+        int age = 2;
+
+        if(age >= 18) {
+            System.out.println("성인입니다.");
+        }else{
+            System.out.println("미성년자입니다.");
+
+        }
+
+    }
+
+}
