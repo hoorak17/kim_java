3489041df3bb8d593f2d9f715c7396b4133d65a8
hoorak17 <hoorak17@naver.com>
2026-04-28 18:42:36 +0900

    반복문

---
+++ src/loop/problem1.java
@@ -0,0 +1,20 @@
+package loop;
+
+public class problem1 {
+    static void main() {
+        int count = 1;
+        for(int i = 0; i < 10;++i ){
+            System.out.println(count*2);
+            count++;
+        }
+
+        count =1;
+        while(count <= 10){{
+            System.out.println(count*2);
+            count++;
+        }
+        }
+
+
+    }
+}
