961b2a2ed3f07437dba8b0de9e15e85c11a6e43b
hoorak17 <hoorak17@naver.com>
2026-07-05 20:39:16 +0900

    0705

--- src/array/array3.java
+++ src/array/array3.java
@@ -1,17 +1,11 @@
 package array;
 
-public class array2 {
+public class array3 {
     static void main() {
-        int[] students;
-        students = new int[5];
+        int[] students = {90,80,70,60,50, 40, 50};
 
         System.out.println(students.length);
 
-        students[0] = 90;
-        students[1] = 80;
-        students[2]= 70;
-        students[3] = 60;
-        students[4] = 50;
 
         for(int i = 0; i < students.length; i++){
             System.out.println("학생 " + ( i+ 1 ) + " 점수 : " + students[i]);
